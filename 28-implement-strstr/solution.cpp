3d1bca85f429fb98913545a969ce13dafaee9c7c
darxsys <darxsys@gmail.com>
2019-02-02 20:28:01 -0800

    strstr

---
+++ 28-implement-strstr/solution.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+
+        int m = haystack.size();
+        int n = needle.size();
+
+        if (n > m) return -1;
+
+        for (int i = 0; i < m; ++i)
+        {
+            int j = 0;
+            for (; j < n; ++j)
+            {
+                if (haystack[i+j] != needle[j]) break;
+            }
+
+            if (j == n)
+                return i;
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+    Solution s;
+    assert(s.strStr("hello", "ll") == 2);
+    assert(s.strStr("aaaaa", "bba") == -1);
+    assert(s.strStr("mississippi", "mississippi") == 0);
+    return 0;
+}
