df2faee3b076c4d7115b32ae086178c964f1d118
darxsys <darxsys@gmail.com>
2019-02-09 21:24:32 -0800

    Lowest common ancestor binary tree

---
+++ 236-lowest-common-ancestor-binary-tree/solution.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left != nullptr && right != nullptr)
+        {
+            return root;
+        }
+        else
+        {
+            return left ? left : right;
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
