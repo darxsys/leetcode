ef32a116cb0c1d9a04ed42ff9737d0e63236c4e9
darxsys <darxsys@gmail.com>
2019-03-06 18:36:38 -0800

    Adding a much nicer and faster solution for 273

--- 273-integer-to-english-words/solution.cpp
+++ 273-integer-to-english-words/solution.cpp
@@ -14,6 +14,154 @@
 #include <algorithm>
 using namespace std;
 
+class Solution2 {
+public:
+    string numberToWords(int num) {
+        if (num == 0) return "Zero";
+
+        int billion = num / 1000000000;
+        int million = (num % 1000000000) / 1000000;
+        int thousand = ((num % 1000000)) / 1000;
+        int hundred = num % 1000;
+
+        string sol = "";
+        string tmp = "";
+        three(billion, tmp);
+        if (!tmp.empty())
+            sol = tmp + " Billion";
+
+        tmp = "";
+        three(million, tmp);
+        if (!tmp.empty())
+        {
+            if (!sol.empty())
+                sol = sol + " " + tmp + " Million";
+            else
+                sol = tmp + " Million";
+        }
+
+        tmp = "";
+        three(thousand, tmp);
+        if (!tmp.empty())
+        {
+            if (!sol.empty())
+                sol = sol + " " + tmp + " Thousand";
+            else
+                sol = tmp + " Thousand";
+        }        
+
+        tmp = "";
+        three(hundred, tmp);
+        if (!tmp.empty())
+        {
+            if (!sol.empty())
+                sol = sol + " " + tmp;
+            else
+                sol = tmp;
+        }
+
+        return sol;
+    }
+private:
+    void three(int num, string& out)
+    {
+        if (num == 0) return;
+
+        int hundreds = num / 100;
+        if (hundreds > 0)
+        {
+            out = ones(hundreds) + " Hundred";
+        }
+
+        int ten = num % 100;
+        string tmp = "";
+        two(ten, tmp);
+
+        if (!tmp.empty())
+        {
+            if (!out.empty())
+                out = out + " " + tmp;
+            else
+                out = tmp;
+        }
+    }
+
+    void two(int num, string& out)
+    {
+        if (num == 0) return;
+
+        int ten = num / 10;
+        if (ten == 1)
+        {
+            out = teens(num);
+        }
+        else if (ten == 0)
+        {
+            out = ones(num);
+        }
+        else
+        {
+            out = tens(num / 10);
+            if (num % 10 != 0) out = out + " " + ones(num % 10);
+        }
+    }
+
+    string ones(int num)
+    {
+        switch (num)
+        {
+        case 0: return "Zero";
+        case 1: return "One";
+        case 2: return "Two";
+        case 3: return "Three";
+        case 4: return "Four";
+        case 5: return "Five";
+        case 6: return "Six";
+        case 7: return "Seven";
+        case 8: return "Eight";
+        case 9: return "Nine";
+        }
+
+        return "";
+    }
+
+    string teens(int num)
+    {
+        switch (num)
+        {
+        case 10: return "Ten";
+        case 11: return "Eleven";
+        case 12: return "Twelve";
+        case 13: return "Thirteen";
+        case 14: return "Fourteen";
+        case 15: return "Fifteen";
+        case 16: return "Sixteen";
+        case 17: return "Seventeen";
+        case 18: return "Eighteen";
+        case 19: return "Nineteen";
+        }
+
+        return "";
+    }
+
+    string tens(int num)
+    {
+        switch (num)
+        {
+        case 2: return "Twenty";
+        case 3: return "Thirty";
+        case 4: return "Forty";
+        case 5: return "Fifty";
+        case 6: return "Sixty";
+        case 7: return "Seventy";
+        case 8: return "Eighty";
+        case 9: return "Ninety";
+        }
+
+        return "";
+    }
+};
+
 class Solution {
 public:
     string numberToWords(int num) {
@@ -80,12 +228,12 @@ private:
 
 int main()
 {
-    Solution s;
-    // printf("%s\n", s.numberToWords(12).c_str());
-    // printf("%s\n", s.numberToWords(123).c_str());
-    // printf("%s\n", s.numberToWords(12345).c_str());
-    // printf("%s\n", s.numberToWords(1234567).c_str());
-    // printf("%s\n", s.numberToWords(1234567891).c_str());
+    Solution2 s;
+    printf("%s\n", s.numberToWords(12).c_str());
+    printf("%s\n", s.numberToWords(123).c_str());
+    printf("%s\n", s.numberToWords(12345).c_str());
+    printf("%s\n", s.numberToWords(1234567).c_str());
+    printf("%s\n", s.numberToWords(1234567891).c_str());
     printf("%s\n", s.numberToWords(123000111).c_str());
     printf("%s\n", s.numberToWords(1012003).c_str());
     printf("%s\n", s.numberToWords(12345).c_str());
