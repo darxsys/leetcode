6785037041de8943243f7dcd5b5d70be75c655a3
darxsys <darxsys@gmail.com>
2020-12-07 18:42:55 -0800

    91

--- 91-decode-ways/solution.cpp
+++ 91-decode-ways/solution.cpp
@@ -42,6 +42,30 @@ private:
     }
 };
 
+class Solution2 {
+public:
+    int numDecodings(string s) {
+        if (s.empty() || s[0] == '0') return 0;
+        
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            if (s[i-1] != '0')
+                dp[i] = dp[i-1];
+            
+            if (s[i-2] == '1' || s[i-2] == '2' && s[i-1] <= '6') {
+                dp[i] += dp[i-2];
+            }
+        }
+        
+        return dp[n];
+    }
+};
+
 int main()
 {
     return 0;
