be9e16405980d78190fe38346d0e3d5afa540f99
darxsys <darxsys@gmail.com>
2020-12-15 20:24:15 -0800

    787 improve

--- 787-cheapest-flights/solution.cpp
+++ 787-cheapest-flights/solution.cpp
@@ -3,41 +3,38 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
         if (flights.empty()) return -1;
         
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(K+2, INT_MAX)));
-        vector<vector<bool>> visited(n, vector<bool>(K+2, false));
+        vector<vector<int>> dp(n, vector<int>(K+2, INT_MAX));
+        vector<vector<int>> adj(n, vector<int>(n, -1));
         
         for (int i = 0; i < flights.size(); ++i) {
-            for (int k = 1; k <= K+1; ++k) {
-                dp[flights[i][0]][flights[i][1]][k] = flights[i][2];
-            }
+            adj[flights[i][0]][flights[i][1]] = flights[i][2];
         }
         
-        dfs(visited, dp, src, dst, K+1);
-        return dp[src][dst][K+1] != INT_MAX ? dp[src][dst][K+1] : -1;
+        dfs(adj, dp, src, dst, K+1);
+        return dp[src][K+1] < INT_MAX ? dp[src][K+1] : -1;
     }
     
 private:
-    void dfs(vector<vector<bool>>& visited, 
-             vector<vector<vector<int>>>& dp, int src, int dst, int K) {
+    void dfs(vector<vector<int>>& adj, 
+             vector<vector<int>>& dp, int src, int dst, int K) {
         if (src == dst) {
-            dp[src][dst][K] = 0;
+            dp[src][K] = 0;
             return;
         }
         
         if (K == 0) return;
         
-        if (dp[src][dst][K] != INT_MAX && visited[src][K]) {
+        if (dp[src][K] != INT_MAX) {
             return;
         }
         
-        visited[src][K]= true;
-        
-        for (int i = 0; i < dp[src].size(); ++i) {
-            if (dp[src][i][K] != INT_MAX) {
-                dfs(visited, dp, i, dst, K-1);
+        for (int i = 0; i < adj[src].size(); ++i) {
+            int weight = adj[src][i];
+            if (weight != -1) {
+                dfs(adj, dp, i, dst, K-1);
                 
-                if (dp[i][dst][K-1] != INT_MAX) {
-                    dp[src][dst][K] = min(dp[src][dst][K], dp[src][i][K] + dp[i][dst][K-1]);
+                if (dp[i][K-1] != INT_MAX) {
+                    dp[src][K] = min(dp[src][K], weight + dp[i][K-1]);
                 }
             }
         }
