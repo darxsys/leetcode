5fba55d0efc415344e4d19a7a9fcdf12f4b1dec7
darxsys <darxsys@gmail.com>
2020-12-26 14:02:01 -0800

    123 better

--- 123-buy-sell/solution.cpp
+++ 123-buy-sell/solution.cpp
@@ -3,11 +3,8 @@ public:
     int maxProfit(vector<int>& prices) {
         if (prices.size() <= 1) return 0;
         
-        int result = 0;
         int n = prices.size();
-        
         vector<int> dp(n, 0);
-        vector<int> dp2(n+1, 0);
         
         int minSoFar = prices[0];
         for (int i = 1; i < n; ++i) {
@@ -15,16 +12,16 @@ public:
             minSoFar = min(minSoFar, prices[i]);
         }
         
+        int result = dp[n-1];
+        dp[n-1] = 0;
         int maxSoFar = prices[n-1];
         for (int i = n - 2; i >= 0; --i) {
-            dp2[i] = max(dp2[i+1], maxSoFar - prices[i]);
+            result = max(result, dp[i] + dp[i+1]);
+            dp[i] = max(dp[i+1], maxSoFar - prices[i]);
             maxSoFar = max(maxSoFar, prices[i]);
         }
-        
-        for (int i = 0; i < n; ++i) {
-            result = max(result, dp[i] + dp2[i+1]);
-        }
-        
+
+        result = max(result, dp[0]);
         return result;
     }
 };
